b03ef62675831917d892684267804ddec5e882f1
atsunset529 <atsunsetree529@gmail.com>
2022-08-01 10:00:39 +0900

    feat: models, serializers

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
@@ -5,11 +6,32 @@ from django.db import models
 
 from accounts.models import User, CustomerUser, ShopUser
 # Create your models here.
+class UserQr(models.Model):
+    user = models.ImageField(upload_to='qrcode', null=True)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    level = models.IntegerField(default=0 ,blank=True)
+    shopname = models.ForeignKey(ShopUser, on_delete=models.CASCADE)
+    coupon_num = models.IntegerField(default=0 ,blank=True)
+    used_at = models.DateTimeField(auto_now_add=True)
+
+class Stamp(models.Model):
+    shopname = models.ForeignKey(ShopUser, on_delete=models.CASCADE)
+    stamp_num = models.IntegerField(default=0, blank=True)
+    
+    def __init__(self):
+        self.stamp_num += 1
+
+class Bookmark(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    shopname = models.ForeignKey(ShopUser, on_delete=models.CASCADE, related_name='bookmark')
+    bookmark_set = models.ManyToManyField(ShopUser, blank=True)
 
 class CustomerQna(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField(max_length=200)
-
 
 class CustomerQuestion(models.Model):
     title = models.CharField(max_length=20)
--- customer/serializers.py
+++ customer/serializers.py
@@ -1,1 +1,28 @@
+from dataclasses import field
+import profile
+from turtle import stamp
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from .models import Bookmark, Profile, Stamp
+from accounts.models import User, ShopUser
+
+class ProileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = [
+            "user", "level", "shopname", "coupon_num", "used_at"
+        ]
+
+class StampSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stamp
+        field = [
+            "shopname", "stamp_num"
+            ]
+    
+class BookmarkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bookmark
+        field = [
+            "shopname", "user"
+            ]
